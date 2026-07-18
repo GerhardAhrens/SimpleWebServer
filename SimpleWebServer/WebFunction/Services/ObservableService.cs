54e6729431e20a0a9fda7fc963c3c82556433dee
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2026-07-18 19:31:23 +0200

    *

---
+++ SimpleWebServer/WebFunction/Services/ObservableService.cs
@@ -0,0 +1,28 @@
+﻿namespace SimpleWebServer.WebFunction.Services
+{
+    using System;
+
+    public class ObservableService<T> where T : class
+    {
+        public event EventHandler Changed;
+
+        public T Current { get; private set; }
+
+        public ObservableService(T initialValue)
+        {
+            this.Current = initialValue;
+        }
+
+        protected void RaiseChanged()
+        {
+            this.Changed?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected void SetCurrent(T value)
+        {
+            this.Current = value;
+
+            this.RaiseChanged();
+        }
+    }
+}
